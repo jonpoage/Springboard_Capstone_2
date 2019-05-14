2df898c3ee6f6a8f61216938af9b0ce495a55ce8
Jon Poage <jonpoage@gmail.com>
2019-05-14 13:57:04 -0700

    draft of model_1 fitting script finished.

--- code/fit_model_1.py
+++ code/fit_model_1.py
@@ -1,4 +1,11 @@
 import models as mdl
+import utils_model_fitting as umf
+import utils_training_history as uth
+from definitions import MODELS_PATH, FIGURE_OUTPUT_PATH
+from keras.optimizers import Adam
+
+# set model name
+model_name = 'model_myCNNbase_myNNclf'
 
 # get model base
 base = mdl.get_my_conv_layers()
@@ -9,3 +16,23 @@ input_dim = base.output_shape[1]
 # get classifier
 clf = mdl.get_my_NN_classifier(input_dim=input_dim,
                                n_output=3)
+
+# compile the model
+model = umf.compile_model(base=base,
+                          classifier=clf,
+                          loss='categorical_crossentropy',
+                          optimizer=Adam(),
+                          metrics=['accuracy'])
+
+# train the model
+output_HDF5_file_path = MODELS_PATH + model_name + '.h5'
+history = umf.fit_and_save_model(model,
+                                 output_file_path=output_HDF5_file_path,
+                                 batch_size=30,
+                                 class_weight=None)
+
+# save figure of training history losses and accuracies
+output_png_file_path = FIGURE_OUTPUT_PATH + model_name \
+                       + '_training_history_plots.png'
+uth.save_training_history_plots(history,
+                                output_file_path=output_png_file_path)
--- code/utils_model_fitting.py
+++ code/utils_model_fitting.py
@@ -1,12 +1,25 @@
 import numpy as np
 import load_processed_data as ld
-from definitions import MODELS_PATH
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 from keras.models import Sequential
 
 
-def compile_model(base, classifier, loss):
+def compile_model(base, classifier, loss='binary_crossentropy',
+                  optimizer=Adam(), metrics=['accuracy']):
+    """This function builds a model and compiles it.
+
+    Input arguments:
+        base - a keras model that takes the image data as input and outputs
+               an array of flattened bottleneck features.
+        classifier - a keras model that takes the array of
+                     flattened bottleneck features as input and outputs
+                     the class predictions.
+        loss - (optional) loss for the model compilation.
+        optimizer - (optional) optimizer for the model compilation.
+        metrics - (optional) list of metrics for the model compilation.
+
+    This function returns a compiled Sequential object."""
 
     # instantiate model
     model = Sequential()
@@ -16,15 +29,31 @@ def compile_model(base, classifier, loss):
     model.add(classifier)
 
     # compile the model
-    model.compile(optimizer=Adam(),
+    model.compile(optimizer=optimizer,
                   loss=loss,
-                  metrics=['accuracy'])
+                  metrics=metrics)
 
     # return the model
     return model
 
 
-def fit_and_save_model(model, str_model_name, batch_size=30):
+def fit_and_save_model(model, output_file_path, batch_size=30,
+                       class_weight=None):
+    """This function fits a model and then saves it to an HDF5 file.
+
+    Input arguments:
+        model - a keras model that has been compiled.
+        output_file_path - pathname of the output .h5 file.
+        batch_size - (optional) batch size for model training, expressed
+                     as an integer.
+            NOTE: this implementation of the function uses the same batch size
+                  for the train and validation data generation.
+        class_weight - (optional) dictionary mapping class indices (integers)
+                       to weight values (floats), to be used during
+                       model training.
+
+    This function returns a History object that contains the
+    model training history."""
 
     # load the train data
     X_train, y_train, _ = ld.load_train_data()
@@ -41,14 +70,14 @@ def fit_and_save_model(model, str_model_name, batch_size=30):
                                        shear_range=0.1,
                                        fill_mode='nearest')
     train_generator = train_datagen.flow(X_train,
-                                         y_train,
+                                         y=y_train,
                                          batch_size=batch_size,
                                          seed=10)
 
     # validation image data generator
     val_datagen = ImageDataGenerator(rescale=1./255)
     val_generator = val_datagen.flow(X_val,
-                                     y_val,
+                                     y=y_val,
                                      batch_size=batch_size,
                                      seed=10)
 
@@ -61,9 +90,10 @@ def fit_and_save_model(model, str_model_name, batch_size=30):
                                   validation_steps=np.ceil(X_val.shape[0]
                                                            / batch_size),
                                   verbose=1,
-                                  class_weight=None)
+                                  class_weight=class_weight)
 
     # save model to HDF5 file
-    model.save(MODELS_PATH + str_model_name + '.h5')
+    model.save(output_file_path)
 
+    # return the training history
     return history
